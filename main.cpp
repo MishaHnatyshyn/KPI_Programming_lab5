2fc7ddebe8eefb1c92ec41a3a225f94b9c9092e4
Misha Hnatyshyn <31630903+MishaHnatyshyn@users.noreply.github.com>
2018-05-05 16:27:35 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+typedef struct {
+    unsigned char  b1, b2;      //Символы BM (смение 0, длина 2)
+    unsigned short  bfSize;      //Размер файла в байтах  (Смещение 2, длина 2)
+    unsigned short  bfReserved1;          //Бесполезно (Смещение 6, длина 2)
+    unsigned short  bfReserved2;          //Бесполезно (Смещение 8, длина 2)
+    unsigned short  padding;    //Мусор, для выравнивания
+    unsigned short  bfOffBits;
+} BM;
+
+typedef struct{
+    int32_t infoSize;
+    int32_t width;
+    int32_t depth;
+    int16_t biPlanes;
+    int16_t bits;
+    int32_t biCompression;
+    int32_t biSizeImage;
+    int32_t biXPelsPerMeter;
+    int32_t biYPelsPerMeter;
+    int32_t biClrUsed;
+    int32_t biClrImportant;
+}  BM2;
+
+
+struct PIXELDATA{
+    int8_t redComponent;
+    int8_t blueComponent;
+    int8_t greenComponent;
+};
+
+int main() {
+    int resize_count = 7;
+
+    FILE * file;
+    file = fopen("/home/mhnatyshyn/Programming_labs/Lab5/bmp.bmp","rb");
+
+    BM bm;
+    BM2 bm2;
+    fread(&bm, 1, 14, file);
+    fread(&bm2, 1, 40, file);
+    
+    int bytesPerLine;
+    bytesPerLine = bm2.width * 3;
+    if (bytesPerLine & 0x0003)
+    {
+        bytesPerLine |= 0x0003;
+        ++bytesPerLine;
+    }
+
+    FILE * output;
+    output = fopen("/home/mhnatyshyn/Programming_labs/Lab5/output.bmp","wb");
+
+    PIXELDATA map[bm2.depth][bm2.width];
+    PIXELDATA map2[bm2.depth * resize_count][bm2.width * resize_count];
+
+    int new_bytesPerLine;
+
+    new_bytesPerLine = bm2.width * resize_count * 3;
+    if (new_bytesPerLine & 0x0003)
+    {
+        new_bytesPerLine |= 0x0003;
+        ++new_bytesPerLine;
+    }
+
+    bm.bfSize=54+(long)new_bytesPerLine*bm2.depth * resize_count;
+
+    const char null_byte = ' ';
+    char garbage;
+    int input_padding = bytesPerLine - bm2.width*3; // count of null byte at the end of each row
+
+    //pixel map reading
+
+    for (int y = bm2.depth-1; y >= 0; y--) {
+        for (int x = 0; x < bm2.width; x++) {
+            fread(&map[y][x],1,3,file);
+        }
+        if(input_padding != 0) fread(&garbage,1,input_padding,file);
+    }
+
+    //new matrix creating
+    
+    int p = 0;
+    int s = 0;
+    for (int i = 0; i < bm2.depth; ++i) {
+        for (int l = 0; l < resize_count; ++l) {
+            for (int j = 0; j < bm2.width; ++j) {
+                for (int k = 0; k < resize_count; ++k) {
+                    map2[p][s] = map[i][j];
+                    s++;
+                }
+            }
+            s = 0;
+            p++;
+        }
+    }
+
+
+    bm2.depth *= resize_count;
+    bm2.width *= resize_count;
+
+
+    fwrite(&bm, 1, 14, output);
+    fwrite(&bm2, 1, 50, output);
+
+    int output_padding = new_bytesPerLine - bm2.width*3; // count of null byte at the end of each row
+    for (int y = bm2.depth-1; y >= 0; y--) {
+        for (int x = 0; x < bm2.width; x++) {
+            fwrite(&map2[y][x],1,3,output);
+        }
+        if(output_padding != 0) fwrite(&null_byte,1,output_padding,output);
+    }
+    
+    fclose(output);
+    fclose(file);
+
+    return 0;
+}
