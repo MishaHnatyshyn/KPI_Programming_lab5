e3e873d40f176d85e2395781f75ea61132d94ac9
MishaHnatyshyn <gnatyshynmisha@gmail.com>
2018-05-12 00:52:01 +0300

    Code refractoring

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <stdint.h>
+#include <cmath>
 
 using namespace std;
 
@@ -34,13 +36,6 @@ struct PIXELDATA{
     uint8_t g;
 };
 
-PIXELDATA get_interpolated_pixel(PIXELDATA Fx0, PIXELDATA Fx1, double x0, double x1, int x){
-    PIXELDATA pixel;
-    pixel.r = (int) floor(Fx0.r + ((Fx1.r - Fx0.r) / (x1 - x0)) * (x - x0));
-    pixel.g = (int) floor(Fx0.g + ((Fx1.g - Fx0.g) / (x1 - x0)) * (x - x0));
-    pixel.b = (int) floor(Fx0.b + ((Fx1.b - Fx0.b) / (x1 - x0)) * (x - x0));
-    return pixel;
-}
 
 int byte_per_line(int width){
     int result;
@@ -53,16 +48,23 @@ int byte_per_line(int width){
     return result;
 }
 
+// Interpolating function
+
+PIXELDATA get_interpolated_pixel(PIXELDATA Fx0, PIXELDATA Fx1, double x0, double x1, int x){
+    PIXELDATA pixel;
+    pixel.r = (int) floor(Fx0.r + ((Fx1.r - Fx0.r) / (x1 - x0)) * (x - x0));
+    pixel.g = (int) floor(Fx0.g + ((Fx1.g - Fx0.g) / (x1 - x0)) * (x - x0));
+    pixel.b = (int) floor(Fx0.b + ((Fx1.b - Fx0.b) / (x1 - x0)) * (x - x0));
+    return pixel;
+}
 
 int main(int argc, char* argv[]) {
     char * input_path = argv[1];
     char * output_path = argv[2];
     double resize = atof(argv[3]);
-    
-    int resize_count = 7;
 
     FILE * file;
-    file = fopen("/home/mhnatyshyn/Programming_labs/Lab5/bmp.bmp","rb");
+    file = fopen(input_path,"rb");
 
     BITMAPFILEHEADER bm;
     BITMAPINFOHEADER bm2;
@@ -71,10 +73,6 @@ int main(int argc, char* argv[]) {
     fread(&bm2, 1, 40, file);
 
     int bytesPerLine = byte_per_line(bm2.width);
-
-    FILE * output;
-    output = fopen("/home/mhnatyshyn/Programming_labs/Lab5/output.bmp","wb");
-
     int new_depth = (int)floor(bm2.depth*resize);
     int new_width = (int)floor(bm2.width*resize);
 
@@ -82,10 +80,8 @@ int main(int argc, char* argv[]) {
     PIXELDATA map2[bm2.depth][new_width];
     PIXELDATA map3[new_depth][new_width];
 
-    int new_bytesPerLine = byte_per_line(bm2.width * resize_count);
-
-    bm.bfSize=54+(long)new_bytesPerLine*bm2.depth * resize_count;
-
+    int new_bytesPerLine = byte_per_line(new_width);
+    bm.bfSize=54+(long)new_bytesPerLine*(new_depth);
     const char null_byte = ' ';
     char garbage;
     int input_padding = bytesPerLine - bm2.width*3; // count of null byte at the end of each row
@@ -99,23 +95,29 @@ int main(int argc, char* argv[]) {
         if(input_padding != 0) fread(&garbage,1,input_padding,file);
     }
 
-    //new matrix creating
+    fclose(file);
+
+    //new pixel map creating
+
+    //interpolation by X
 
     for (int y = 0; y < bm2.depth; y++) {
-            int i = 0;
-            for (int x = 0; x < bm2.width; x++) {
-                double x0= x*resize;
-                double x1= (x+1)*resize;
-                if(x0 == i || x1 == i){
-                    map2[y][i] = map[y][x];
-                    i++;
-                }
-                while (i > x0 && i < x1) {
-                    map2[y][i] = get_interpolated_pixel(map[y][x], map[y][x+1], x0, x1, i);
-                    i++;
-                }
+        int i = 0;
+        for (int x = 0; x < bm2.width; x++) {
+            double x0= x*resize;
+            double x1= (x+1)*resize;
+            if(x0 == i || x1 == i){
+                map2[y][i] = map[y][x];
+                i++;
+            }
+            while (i > x0 && i < x1) {
+                map2[y][i] = get_interpolated_pixel(map[y][x], map[y][x+1], x0, x1, i);
+                i++;
             }
         }
+    }
+
+    //interpolation by Y
 
     for (int x = 0; x < new_depth; x++) {
         int j = 0;
@@ -133,24 +135,23 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    bm2.depth = new_depth;
+    bm2.width = new_width;
 
-    bm2.depth *= resize_count;
-    bm2.width *= resize_count;
-
+    FILE * output;
+    output = fopen(output_path,"wb");
 
     fwrite(&bm, 1, 14, output);
-    fwrite(&bm2, 1, 50, output);
+    fwrite(&bm2, 1, 40, output);
 
     int output_padding = new_bytesPerLine - bm2.width*3; // count of null byte at the end of each row
     for (int y = bm2.depth-1; y >= 0; y--) {
         for (int x = 0; x < bm2.width; x++) {
-            fwrite(&map2[y][x],1,3,output);
+            fwrite(&map3[y][x],1,3,output);
         }
         if(output_padding != 0) fwrite(&null_byte,1,output_padding,output);
     }
-
     fclose(output);
-    fclose(file);
 
     return 0;
 }
